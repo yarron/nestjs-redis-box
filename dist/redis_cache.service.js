552d29cb93db5cd1e913861bbda94fe5d7142007
Yaroslaw Romanov <yaroslaw.romanov@architech.nyc>
2023-12-05 18:19:02 +0300

    Fix redis and build

--- dist/redis_cache.service.js
+++ dist/redis_cache.service.js
@@ -17,7 +17,7 @@ let RedisCacheService = class RedisCacheService extends ioredis_1.default {
         super(options);
     }
     async get(key) {
-        return JSON.parse((await super.get(key)) || '');
+        return JSON.parse((await super.get(key)) || '{}');
     }
     async set(key, value) {
         return super.set(key, JSON.stringify(value));
